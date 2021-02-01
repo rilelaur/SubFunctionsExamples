c3fb29affeea869d2ac9ad8efaf9e47807f4f2b8
Laura <laurakriley01@gmail.com>
2021-02-01 08:40:16 -0700

    RunningTotal

---
+++ RunningTotal.vb
@@ -0,0 +1,24 @@
+﻿Option Strict On
+Option Explicit On
+
+Module RunningTotal
+    Sub main()
+        For i = 0 To 2
+            Console.WriteLine(RunningTotal(5, False))
+        Next
+        Console.WriteLine(RunningTotal(17, True))
+        Console.WriteLine(RunningTotal(82, False))
+        Console.Read()
+    End Sub
+
+    Function RunningTotal(newAmount As Integer, clear As Boolean) As Integer
+        Static _runningTotal As Integer
+        _runningTotal += newAmount
+        If clear = True Then
+            _runningTotal = 0
+        End If
+
+        Return _runningTotal
+    End Function
+
+End Module
---
+++ SpecialFunc.vb
@@ -0,0 +1,43 @@
+﻿Option Explicit On
+Option Strict On
+
+Module SpecialFunc
+    Sub Main()
+
+        PromptUser("hello user")
+
+        For i = 0 To 5
+            StoreMessages("hello")
+        Next
+
+        PromptUser(StoreMessages(""))
+
+        Console.Read()
+    End Sub
+
+    Sub PromptUser(message As String)
+        Console.WriteLine(message)
+    End Sub
+
+    Function StoreMessages(message As String) As String
+        Static storedMessages As String 'Static extrends the lifetime of the variable
+
+        storedMessages &= message & vbCrLf
+
+        Return storedMessages
+    End Function
+
+    Function RandomNumberInRange(maxNumber As Integer) As Single
+        Dim value As Integer
+        Dim temp As Single
+        ' Initialize the random-number generator.
+        Randomize(DateTime.Now.Millisecond)
+
+        'value = CInt(Int((6 * Rnd()) + 1))
+        temp = Rnd()
+        Console.WriteLine($"Rnd:{temp} Cint:{CInt(temp)} Int:{Int(temp)}")
+        Return temp
+
+    End Function
+
+End Module
